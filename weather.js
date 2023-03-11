a594c40dea6427cccace0fdd0733a498e61fafd5
prabalsengar1998 <prabalsengar1998@gmail.com>
2023-03-11 22:24:27 +0530

    added ejs

--- weather.js
+++ weather.js
@@ -1,42 +1,35 @@
 const express=require("express")
 const https=require("https")
-const bodyParser=require("body-parser")
+const bodyparser=require("body-parser")
 const chalk=require("chalk")
+const ejs=require("ejs")
+
 const app=express()
 
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyparser.urlencoded({extended:true}))
 app.use(express.static("public"))
+app.set("view engine","ejs")
 
 app.get("/",function(req,res){
-     res.sendFile(__dirname+"/weather.html")    
+    res.sendFile(__dirname+"/weather.html")
 })
 
 app.post("/",function(req,res){
     var city=req.body.cityName
-    var apiKey="1f94a43222470abf2b8a85be07bf7693"
-
-    var url="https://api.openweathermap.org/data/2.5/weather?q="+city+",India&appid="+apiKey+"&units=metric"
+    const apiKey="1f94a43222470abf2b8a85be07bf7693"
+    var url="https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+apiKey+"&units=metric"
     https.get(url,function(response){
-            response.on("data",function(data){
-                var weatherData=JSON.parse(data)
-                var description=weatherData.weather[0].description
-                var temperature=weatherData.main.temp
-                var icon=weatherData.weather[0].icon
-
-                var imageUrl="http://openweathermap.org/img/wn/"+icon+"@2x.png"
-
-                res.write("<h1>desription: "+description+"</h1>")       
-                res.write("<h1>temperature: "+temperature+" c</h1>")
-                res.write("<img src="+imageUrl+">")
-                res.send()
-            })
+        response.on("data",function(data){
+            var weatherData=JSON.parse(data)
+            var temp=weatherData.main.temp
+            var description=weatherData.weather[0].description
+            var imageCode=weatherData.weather[0].icon
+            var imageUrl="https://openweathermap.org/img/wn/"+imageCode+"@2x.png"
+            
+            res.render("weather",{city:city,temp:temp,description:description,imageUrl:imageUrl})
+        })
     })
-
 })
-const port=process.env.PORT||5000
-
-app.listen(port,function(){
-    console.log(chalk.green.inverse("server is running on port"+port));
+app.listen(3000,function(){
+    console.log(chalk.green.inverse("server is running on port 3000"))
 })
-
-
